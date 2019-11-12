06570fd52114a1240b57444d295845f29f0536c7
Jeffrey Sojack <jeffrey.sojack@gmail.com>
2019-11-12 16:25:21 -0500

    added the first version

--- public/main.js
+++ public/main.js
@@ -1,7 +1,37 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+const getWeather = async () => {
+	let urlString
+	let test = document.getElementById('weather-input').value
+	console.log(test)
+	let x = parseInt(test)
+	console.log(x)
+	if (isNaN(x)) {
+		console.log('made it')
+		urlString =
+			'https://api.openweathermap.org/data/2.5/weather?q=' +
+			test +
+			'&units=imperial&APPID=4530c55b5cfdc2b0beae825af0524c52'
+
+		console.log('url: ' + urlString)
+	} else if (Number.isInteger(x)) {
+		console.log('number is')
+		urlString =
+			'https://api.openweathermap.org/data/2.5/weather?zip=' +
+			test +
+			'&,us&units=imperial&APPID=4530c55b5cfdc2b0beae825af0524c52'
+	}
+	console.log('going to API')
+	document.querySelector('.weather').textContent = ''
+	let response = await fetch(urlString)
+	console.log('back from API')
+	console.log(response)
+	weatherData = await response.json()
+	displayData(weatherData)
 }
 
-document.addEventListener('DOMContentLoaded', main)
+const displayData = (weatherData) => {
+	console.log(weatherData.weather[0].main)
+	document.querySelector('.weather').textContent =
+		weatherData.weather[0].description + ' with a temperature of: ' + weatherData.main.temp + ' degrees F'
+}
+
+document.querySelector('.get-weather').addEventListener('click', getWeather)
